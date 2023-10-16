649f00bb8ba8b71125fdeb85b7bffe512a081116
Iago Passaglia <iago210669@gmail.com>
2023-10-16 17:50:21 -0300

    Criação do menu lateral drop box e Gráfico em linha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+
+import { SobreComponent } from './sobre/sobre/sobre.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SobreComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
